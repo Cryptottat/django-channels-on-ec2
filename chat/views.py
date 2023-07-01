a47c66d9ca4cc6df7b2ef2d9476a414d6a90ecf6
morgan-b-kim <morgan-b-kim@gmail.com>
2023-07-02 05:11:22 +0900

    requirements.txt changed

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from chat.models import Room
+from django.views.generic import TemplateView
+
+class Home(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        # 단순히 시작히 룸을 만들기 위한 장치
+        Room.objects.get_or_create(name='노티룸', group_name='main')
+        return super().get(request, *args, **kwargs)
