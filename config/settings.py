eb851103b2fd4f103078e063149fc3d5ad3efebf
morgan-b-kim <morgan-b-kim@gmail.com>
2023-07-02 04:56:58 +0900

    changed

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    "channels",
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -91,7 +91,7 @@ CHANNEL_LAYERS = {
      },
  }
 
-# WSGI_APPLICATION = "config.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
