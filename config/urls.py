012ccfcc688300f76cd7dfc9526678f040404cdf
morgan-b-kim <morgan-b-kim@gmail.com>
2023-07-02 05:32:49 +0900

    re

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from chat.views import Notification
 urlpatterns = [
     path("admin/", admin.site.urls),
 path('notification/', Notification.as_view())
