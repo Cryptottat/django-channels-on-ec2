278eaa269fec67f32b63a5c26c1bd2b35a9133c2
morgan-b-kim <morgan-b-kim@gmail.com>
2023-07-02 06:11:07 +0900

    re

--- config/asgi.py
+++ config/asgi.py
@@ -17,6 +17,6 @@ import django
 from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
-# application = get_default_application()
+application = get_default_application()
 
-application = get_asgi_application()
+# application = get_asgi_application()
