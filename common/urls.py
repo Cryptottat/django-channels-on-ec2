c39d1b4125b4b34d9fc9d069ec79599c137fbe0f
morgan-b-kim <morgan-b-kim@gmail.com>
2023-07-02 08:54:49 +0900

    test

--- common/urls.py
+++ common/urls.py
@@ -4,6 +4,31 @@ from . import views
 
 app_name = "common"
 
+
+
+from django.contrib.auth.views import LoginView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.debug import sensitive_post_parameters
+from django.http import HttpResponseRedirect,HttpResponse
+
+class DangerousLoginView(LoginView):
+    '''A LoginView with no CSRF protection.'''
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(csrf_exempt)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            redirect_to = self.get_success_url()
+            if redirect_to == self.request.path:
+                raise ValueError(
+                    'Redirection loop for authenticated user detected. Check that '
+                    'your LOGIN_REDIRECT_URL doesn\'t point to a login page.')
+            return HttpResponseRedirect(redirect_to)
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('login/', DangerousLoginView.as_view(template_name='common/login.html'), name='login'),
 ]
