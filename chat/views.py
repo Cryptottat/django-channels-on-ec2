742d1dfeea958c7a8a436fa91775cfcf8f25852a
morgan-b-kim <morgan-b-kim@gmail.com>
2023-07-02 05:30:45 +0900

    config/routing.py application add http

--- chat/views.py
+++ chat/views.py
@@ -5,6 +5,12 @@ from django.shortcuts import render
 from chat.models import Room
 from django.views.generic import TemplateView
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 class Home(TemplateView):
     template_name = 'index.html'
 
@@ -12,3 +18,21 @@ class Home(TemplateView):
         # 단순히 시작히 룸을 만들기 위한 장치
         Room.objects.get_or_create(name='노티룸', group_name='main')
         return super().get(request, *args, **kwargs)
+
+
+class Notification(APIView):
+    def post(self, request, *args, **kwargs):
+        room = Room.objects.get(group_name='main')
+        message = room.messages.create(message=request.data.get('message'))
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'main',
+            {
+                'type': 'chat_message',
+                'message': str(message),
+                'created': message.created.strftime('%p %I:%M')
+            }
+        )
+
+        return Response({'status': 200, 'meesage': '{} send success'.format(message)})
