e413e8e5509de51fa20dcbf92ccf1acc9abd2aea
morgan-b-kim <morgan-b-kim@gmail.com>
2023-07-02 16:34:45 +0900

    test

--- common/urls.py
+++ common/urls.py
@@ -43,6 +43,6 @@ urlpatterns = [
     # path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', DangerousLoginView.as_view(template_name='common/login.html'), name='login'),
-    path('logout/', DangerousLogoutView.LogoutView.as_view(), name='logout'),
+    path('logout/', DangerousLogoutView.as_view(), name='logout'),
 
 ]
